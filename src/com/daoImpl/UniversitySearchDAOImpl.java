36c0bfc7066a370d011c47b8e2e2f2dd0da940c6
hemanth.vs12 <hemanth.vs12@gmail.com>
2015-03-18 20:30:26 +0000

    Sprint-1 Setup

---
+++ src/com/daoImpl/UniversitySearchDAOImpl.java
@@ -0,0 +1,14 @@
+package com.daoImpl;
+
+import com.dao.UniversitySearchDAO;
+import com.dto.UniversityDTO;
+
+public class UniversitySearchDAOImpl implements UniversitySearchDAO{
+
+	@Override
+	public UniversityDTO fetchUniversityDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
