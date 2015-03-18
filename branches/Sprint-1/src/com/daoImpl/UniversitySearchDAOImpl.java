f794a678868a8a3d2052319d22d38ddf91e3ebcc
hemanth.vs12 <hemanth.vs12@gmail.com>
2015-03-18 20:43:01 +0000

    Add new methods.

--- branches/Sprint-1/src/com/daoImpl/UniversitySearchDAOImpl.java
+++ branches/Sprint-1/src/com/daoImpl/UniversitySearchDAOImpl.java
@@ -1,13 +1,26 @@
 package com.daoImpl;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
+import com.connection.MySQLConnection;
 import com.dao.UniversitySearchDAO;
 import com.dto.UniversityDTO;
 
-public class UniversitySearchDAOImpl implements UniversitySearchDAO{
+public class UniversitySearchDAOImpl  extends MySQLConnection implements UniversitySearchDAO{
 
 	@Override
 	public UniversityDTO fetchUniversityDetails() {
-		// TODO Auto-generated method stub
+		Connection conn = null;
+		Statement stmt = null;
+		StringBuffer query = null;
+		try{
+			conn = getConnection();
+			stmt = conn.createStatement();
+		    //stmt.executeUpdate(query);
+		}catch(Exception e){
+			
+		}
 		return null;
 	}
 
