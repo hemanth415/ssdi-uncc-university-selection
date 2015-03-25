ce4d90bfaa8c2658619f864d1ba565f659c6ff26
hemanth.vs12 <hemanth.vs12@gmail.com>
2015-03-25 15:51:12 +0000

    Added validation for Search University

--- branches/Sprint-1/src/com/controller/SearchUniversityController.java
+++ branches/Sprint-1/src/com/controller/SearchUniversityController.java
@@ -13,66 +13,87 @@ import javax.servlet.http.HttpServletResponse;
 import com.dao.SearchUniversityDAO;
 import com.daoImpl.SearchUniversityDAOImpl;
 import com.dto.UniversityDTO;
+import com.utils.ValidatorUtils;
 
 /**
- * Servlet implementation class SearchUniversityServlet
+ * Servlet implementation class SearchUniversityController
+ * 
+ * @author Hemanth
+ *
  */
 @WebServlet("/SearchUniversityController")
 public class SearchUniversityController extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
-    private SearchUniversityDAO searchUniversityDAO = null;   
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchUniversityController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private SearchUniversityDAO searchUniversityDAO = null;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public SearchUniversityController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//if(session != null){
-		System.out.println("U Country:     "+request.getParameter("uCountry"));
-		System.out.println("U State:     "+request.getParameter("uState"));
-		//if(request.getParameter("universityName") != null){
-			UniversityDTO universityDTO = new UniversityDTO();
-			boolean result = false;
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// if(session != null){
+		System.out.println("U Country:     " + request.getParameter("uCountry"));
+		System.out.println("U State:     " + request.getParameter("uState"));
+		UniversityDTO universityDTO = new UniversityDTO();
+		boolean result = false;
+		searchUniversityDAO = new SearchUniversityDAOImpl();
+		List<UniversityDTO> resultList = null;
+		String message = null;
+		if (ValidatorUtils.validateString(request.getParameter("uName"))) {
 			searchUniversityDAO = new SearchUniversityDAOImpl();
-			List<UniversityDTO> resultList = null;
-			if(request.getParameter("uName") != null && !request.getParameter("uName").isEmpty()){
-				searchUniversityDAO = new SearchUniversityDAOImpl();
-				universityDTO.setUniversityName(request.getParameter("uName"));
-				resultList=searchUniversityDAO.fetchUniversityByName(universityDTO);
-			}else if (request.getParameter("uState") != null && request.getParameter("uCountry") != null){
-				searchUniversityDAO = new SearchUniversityDAOImpl();
-				universityDTO.setUniversityState(request.getParameter("uState"));
-				universityDTO.setUniversityCountry(request.getParameter("uCountry"));
-				resultList=searchUniversityDAO.fetchUniversitiesByCntryState(universityDTO);
-			}
-			RequestDispatcher rd = null;
-			if(!resultList.isEmpty()){
-				for(UniversityDTO dto:resultList){
-					System.out.println("Name: "+dto.getUniversityName());
+			universityDTO.setUniversityName(request.getParameter("uName").trim());
+			resultList = searchUniversityDAO
+					.fetchUniversityByName(universityDTO);
+		} else if (ValidatorUtils.validateString(request.getParameter("uCountry"))
+				&& ValidatorUtils.validateString(request.getParameter("uState"))) {
+			searchUniversityDAO = new SearchUniversityDAOImpl();
+			if (ValidatorUtils.validateCountry(request.getParameter("uCountry"))) {
+				if (ValidatorUtils.validateCountryState(
+						request.getParameter("uCountry").trim(), request.getParameter("uState").trim())) {
+					universityDTO.setUniversityState(request.getParameter("uState").trim());
+					universityDTO.setUniversityCountry(request.getParameter("uCountry").trim());
+					resultList = searchUniversityDAO.fetchUniversitiesByCntryState(universityDTO);
+				} else {
+					message = "Invalid state for the provided country";
 				}
-				result = true;
+			} else {
+				message = "Invalid country";
 			}
-			request.setAttribute("result", result);
-			request.setAttribute("uniList", resultList);
-			rd = getServletContext().getRequestDispatcher("/universitySearch.jsp");
-			rd.forward(request, response);
-			
-		//}
-		//}
+		} else {
+			message = "Provide valid university or combination of country and state";
+		}
+		RequestDispatcher rd = null;
+		if (resultList !=null && !resultList.isEmpty()) {
+			for (UniversityDTO dto : resultList) {
+				System.out.println("Name: " + dto.getUniversityName());
+			}
+			message = "Obtained search result: " + resultList.size();
+			result = true;
+		}
+		request.setAttribute("result", result);
+		request.setAttribute("uniList", resultList);
+		request.setAttribute("message", message);
+		rd = getServletContext().getRequestDispatcher("/universitySearch.jsp");
+		rd.forward(request, response);
 	}
 
 }
