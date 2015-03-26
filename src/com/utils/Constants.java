14cd9c2bd2aeeb15bdbb2be443b0029c5e638490
hemanth.vs12 <hemanth.vs12@gmail.com>
2015-03-26 22:56:05 +0000

    Bug Fix - Related to case sensitivity in search. And JUnit tests for University Search

--- src/com/utils/Constants.java
+++ src/com/utils/Constants.java
@@ -6,8 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Constants {
-	public static String SEARCH_UNIV_BY_COUNTRY_STATE = "select university_name,university_desc,address,contact_no,email_id from universities as a join location as b on a.location_id=b.location_id where country=? and state = ?";
-	public static String SEARCH_UNIV_BY_NAME = "select university_name,university_desc,address,contact_no,email_id from universities where university_name = ?";
+	public static String SEARCH_UNIV_BY_COUNTRY_STATE = "select university_name,university_desc,address,contact_no,email_id from universities as a join location as b on a.location_id=b.location_id where UPPER(b.country)=? and UPPER(b.state) = ?";
+	public static String SEARCH_UNIV_BY_NAME = "select university_name,university_desc,address,contact_no,email_id from universities where UPPER(university_name) = ?";
 	public static ArrayList<String> VALID_COUNTRIES = new ArrayList<String>(
 			Arrays.asList("US", "UNITED STATES", "AUSTRALIA", "INDIA")
 			);
