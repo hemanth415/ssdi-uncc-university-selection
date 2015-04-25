fa53d7a036f5b0807268116c2caeb92d2ca2566c
hemdec24 <hemdec24@gmail.com>
2015-04-25 16:07:39 +0000

    Wishlist Business Hemchand

---
+++ branches/Sprint-1/src/com/daoImpl/WishListDAOImpl.java
@@ -0,0 +1,70 @@
+package com.daoImpl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.connection.MySQLConnection;
+import com.dao.WishListDAO;
+import com.dto.WishListDTO;
+import com.utils.Constants;
+
+public class WishListDAOImpl extends MySQLConnection implements WishListDAO{
+
+	@Override
+	public boolean saveToWishList(WishListDTO wishListDTO) {
+		// TODO Auto-generated method stub
+		Connection conn = null;
+		PreparedStatement findStatusStatement = null;
+		PreparedStatement wishListInsertStatement = null;
+		PreparedStatement deleteFromInsertStatement = null;
+		ResultSet resultSet = null;
+		int insertStatus, deleteStatus;
+		
+		try{
+			conn = getConnection();
+			
+			findStatusStatement = conn.prepareStatement(Constants.GET_STATUS_WISH_LIST);
+			findStatusStatement.setInt(1, wishListDTO.getPost_id());
+			findStatusStatement.setInt(2, wishListDTO.getUser_id());
+			
+			resultSet = findStatusStatement.executeQuery();
+			
+			if(resultSet.next()){
+				deleteFromInsertStatement = conn.prepareStatement(Constants.DELETE_FROM_WISH_LIST);
+				deleteFromInsertStatement.setInt(1, wishListDTO.getPost_id());
+				deleteFromInsertStatement.setInt(2, wishListDTO.getUser_id());
+				
+				deleteStatus = deleteFromInsertStatement.executeUpdate();		
+				
+				if(deleteStatus == 1){
+					System.out.println("DATA deleted from Wish List");
+					return false;
+				}
+				else{
+					System.out.println("Failed to delete");
+					return true;
+				}
+			} else{
+				wishListInsertStatement = conn.prepareStatement(Constants.INSERT_TO_WISH_LIST);
+				wishListInsertStatement.setInt(1, wishListDTO.getPost_id());
+				wishListInsertStatement.setInt(2, wishListDTO.getUser_id());
+				
+				insertStatus = wishListInsertStatement.executeUpdate();		
+				
+				if(insertStatus == 1){
+					System.out.println("DATA inserted into Wish List");
+					return true;
+				}
+				else{
+					System.out.println("Failed to insert into Wish List");
+					return false;
+				}
+			}
+			
+		} catch(Exception E){
+			System.out.println(E+"");
+		}
+		return false;
+	}
+}
